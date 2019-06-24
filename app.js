d7a1eb05974feaf42c3b44f7bc901f7178dc16d7
chapmanCC <chapboy1989@gmail.com>
2019-06-25 02:24:36 +0800

    error is now logged to console

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.use((req, res, next) => {
 // ================================
 
 app.use((err, req, res, next) => {
+    console.error(err);
     res.status(err.status);
     res.render("error", { err });
     next();
